3aef141bb84499a0dafa479dca63672187d367c0
Linda Mbongue <linda.mbongue@outlook.fr>
2026-07-16 09:26:12 +0000

    Add use_sim_time parameter to the launch file to choose between using and not using the simulation clock

--- cartographer_slam/launch/cartographer.launch.py
+++ cartographer_slam/launch/cartographer.launch.py
@@ -1,23 +1,37 @@
 import os
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration, PythonExpression
 from launch_ros.actions import Node
 from ament_index_python import get_package_share_directory
 
 def generate_launch_description() :
+
+    use_sim_time = LaunchConfiguration("use_sim_time")
+
+    use_sim_time_arg = DeclareLaunchArgument(
+        "use_sim_time", 
+        default_value="True",
+        description="Use the simulation clock when true"
+    )
 
     cartographer_config_dir = os.path.join(
         get_package_share_directory('cartographer_slam'), 
         'config'
     )
 
-    configuration_basename = 'cartographer_sim.lua'
+    configuration_basename = PythonExpression([
+        "'cartographer_sim.lua' if ",
+        use_sim_time,
+        " else 'cartographer_real.lua'",
+    ])
 
     cartographer_node = Node(
         package="cartographer_ros",
         executable="cartographer_node",
         name="cartographer_node",
         output="screen",
-        parameters=[{'use_sim_time': True}],
+        parameters=[{'use_sim_time': use_sim_time}],
         arguments=['-configuration_directory', cartographer_config_dir,
                    '-configuration_basename', configuration_basename]
     )
@@ -27,7 +41,7 @@ def generate_launch_description() :
         executable="cartographer_occupancy_grid_node",
         name="occupancy_grid_node",
         output="screen",
-        parameters=[{"use_sim_time": True}],
+        parameters=[{"use_sim_time": use_sim_time}],
         arguments=[
             "-resolution", "0.05",
             "-publish_period_sec", "1.0"
@@ -48,10 +62,11 @@ def generate_launch_description() :
         name="rviz2",
         output="screen",
         arguments=['-d ', rviz_config_file_path],
-        parameters=[{"use_sim_time" : True}]
+        parameters=[{"use_sim_time" : use_sim_time}]
     )
 
     return LaunchDescription([
+        use_sim_time_arg,
         cartographer_node,
         occupancy_grid_node,
         rviz2_node
