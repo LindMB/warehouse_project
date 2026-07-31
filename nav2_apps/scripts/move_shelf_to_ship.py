09da35a02609798de41d3756a6c7b7f0e5cdf9af
Linda Mbongue <linda.mbongue@outlook.fr>
2026-07-31 11:33:54 +0000

    Add a middle position to reach before shipping_position

--- nav2_apps/scripts/move_shelf_to_ship.py
+++ nav2_apps/scripts/move_shelf_to_ship.py
@@ -161,24 +161,22 @@ def main():
             print('The robot could not leave the loading area.')
             exit(1)
 
-        ### Navigate to the shipping position
-        # Robot Shipping Position (between both tables)
-        shipping_position = PoseStamped()
-        shipping_position.header.frame_id = 'map'
-        shipping_position.header.stamp = navigator.get_clock().now().to_msg()
+        ### Navigate to the middle point of the shipping position
+        # Robot Middle Position (between both tables)
+        middle_position = PoseStamped()
+        middle_position.header.frame_id = 'map'
+        middle_position.header.stamp = navigator.get_clock().now().to_msg()
 
-        # Robot Shipping Position received from /goal_pose topic 
+        # Robot Middle Position received from /goal_pose topic 
         # when 2D Goal Pose is set in RViz
-        shipping_position.pose.position.x = 2.721620101928711
-        shipping_position.pose.position.y = 1.357941198348999
-        shipping_position.pose.orientation.z = 0.735059298028239
-        shipping_position.pose.orientation.w = 0.6780028232848537
+        middle_position.pose.position.x = 2.6824581623077393
+        middle_position.pose.position.y = 0.042989447712898254
+        middle_position.pose.orientation.z = 0.7013630206684542
+        middle_position.pose.orientation.w = 0.7128042601155112
 
-        print('Navigating to shipping_position...')
+        print('Navigating to middle_position...')
 
-        navigator.goToPose(shipping_position)
-
-        i = 0
+        navigator.goToPose(middle_position)
 
         while not navigator.isTaskComplete():
             i = i + 1
@@ -189,81 +187,128 @@ def main():
                     feedback.estimated_time_remaining
                 ).nanoseconds / 1e9
 
-                print('Estimated time to shipping_position: '
+                print('Estimated time to middle_position: '
                     + '{0:.0f}'.format(estimated_time) + ' seconds.'
                 )
 
         result = navigator.getResult()
 
         if result == TaskResult.SUCCEEDED:
-            print('The robot has reached shipping_position.')
+                print('The robot has returned to middle_position.')
 
-            ### Put the elevator down
-            shelf_approach.put_elevator_down()
+                ### Navigate to the shipping position
+                # Robot Shipping Position (between both tables)
+                shipping_position = PoseStamped()
+                shipping_position.header.frame_id = 'map'
+                shipping_position.header.stamp = navigator.get_clock().now().to_msg()
 
-            ### Update footprint shape from robot + shelf to robot only
-            global_footprint_pub = navigator.create_publisher(
-                Polygon,
-                '/global_costmap/footprint',
-                10
-            )
+                # Robot Shipping Position received from /goal_pose topic 
+                # when 2D Goal Pose is set in RViz
+                shipping_position.pose.position.x = 2.6824581623077393 #2.721620101928711
+                shipping_position.pose.position.y = 1.3578556776046753 #1.357941198348999
+                shipping_position.pose.orientation.z = 0.7007256910935754 #0.735059298028239
+                shipping_position.pose.orientation.w = 0.7134307996164948 #0.6780028232848537
 
-            local_footprint_pub = navigator.create_publisher(
-                Polygon,
-                '/local_costmap/footprint',
-                10
-            )
+                print('Navigating to shipping_position...')
 
-            robot_footprint = Polygon()
+                navigator.goToPose(shipping_position)
 
-            # Circular shape definition
-            robot_footprint.points = create_circular_footprint(0.30)
+                i = 0
 
-            print('Restoring the normal robot footprint...')
+                while not navigator.isTaskComplete():
+                    i = i + 1
+                    feedback = navigator.getFeedback()
 
-            for i in range(5):
-                global_footprint_pub.publish(robot_footprint)
-                local_footprint_pub.publish(robot_footprint)
-                time.sleep(0.1)
+                    if feedback and i % 5 == 0:
+                        estimated_time = Duration.from_msg(
+                            feedback.estimated_time_remaining
+                        ).nanoseconds / 1e9
 
-            ### Move out of loading position by moving the robot backward
-            if not shelf_approach.move_out_of_shipping_area(distance=1.2):
-                print('The robot could not leave the shipping area.')
-                exit(1)
+                        print('Estimated time to shipping_position: '
+                            + '{0:.0f}'.format(estimated_time) + ' seconds.'
+                        )
 
-            # Refresh the timestamp before reusing the initial pose.
-            init_position.header.stamp = (
-                navigator.get_clock().now().to_msg()
-            )
+                result = navigator.getResult()
 
-            print('Returning to init_position...')
+                if result == TaskResult.SUCCEEDED:
+                    print('The robot has reached shipping_position.')
 
-            navigator.goToPose(init_position)
+                    ### Put the elevator down
+                    shelf_approach.put_elevator_down()
 
-            while not navigator.isTaskComplete():
-                pass
+                    ### Update footprint shape from robot + shelf to robot only
+                    global_footprint_pub = navigator.create_publisher(
+                        Polygon,
+                        '/global_costmap/footprint',
+                        10
+                    )
 
-            result = navigator.getResult()
+                    local_footprint_pub = navigator.create_publisher(
+                        Polygon,
+                        '/local_costmap/footprint',
+                        10
+                    )
 
-            if result == TaskResult.SUCCEEDED:
-                print('The robot has returned to init_position.')
+                    robot_footprint = Polygon()
 
-            elif result == TaskResult.CANCELED:
-                print('Navigation to init_position was canceled.')
-                exit(1)
+                    # Circular shape definition
+                    robot_footprint.points = create_circular_footprint(0.30)
 
-            elif result == TaskResult.FAILED:
-                print('Navigation to init_position failed.')
-                exit(1)
+                    print('Restoring the normal robot footprint...')
+
+                    for i in range(5):
+                        global_footprint_pub.publish(robot_footprint)
+                        local_footprint_pub.publish(robot_footprint)
+                        time.sleep(0.1)
+
+                    ### Move out of loading position by moving the robot backward
+                    if not shelf_approach.move_out_of_shipping_area(distance=1.2):
+                        print('The robot could not leave the shipping area.')
+                        exit(1)
+
+                    # Refresh the timestamp before reusing the initial pose.
+                    init_position.header.stamp = (
+                        navigator.get_clock().now().to_msg()
+                    )
+
+                    print('Returning to init_position...')
+
+                    navigator.goToPose(init_position)
+
+                    while not navigator.isTaskComplete():
+                        pass
+
+                    result = navigator.getResult()
+
+                    if result == TaskResult.SUCCEEDED:
+                        print('The robot has returned to init_position.')
+
+                    elif result == TaskResult.CANCELED:
+                        print('Navigation to init_position was canceled.')
+                        exit(1)
+
+                    elif result == TaskResult.FAILED:
+                        print('Navigation to init_position failed.')
+                        exit(1)
+
+
+                elif result == TaskResult.CANCELED:
+                    print('Navigation to shipping_position was canceled.')
+                    exit(1)
+
+                elif result == TaskResult.FAILED:
+                    print('Navigation to shipping_position failed.')
+                    exit(1) 
 
 
         elif result == TaskResult.CANCELED:
-            print('Navigation to shipping_position was canceled.')
+            print('Navigation to middle_position was canceled.')
             exit(1)
 
         elif result == TaskResult.FAILED:
-            print('Navigation to shipping_position failed.')
-            exit(1)  
+            print('Navigation to middle_position failed.')
+            exit(1) 
+
 
     elif result == TaskResult.CANCELED:
         print('Navigation to loading_position was canceled.')
